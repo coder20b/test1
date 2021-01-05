ae9701620be4ec21aad4380621ea6b69e4aeb930
LH <lh@applh.com>
2021-01-05 16:53:19 +0100

    ajout lorem

--- contact.php
+++ contact.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>PAGE CONTACT</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quae odit suscipit saepe inventore, numquam sit vitae quam, excepturi, perferendis velit doloremque ullam voluptatibus iure beatae ut modi voluptates dolorem?</p>
 </body>
 </html>
