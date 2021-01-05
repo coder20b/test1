4167744eee82101850fc42181dfa0469c9eaa670
LH <lh@applh.com>
2021-01-05 16:33:46 +0100

    ma 2e sauvegarde

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MON TITRE DE MON SITE</title>
 </head>
 <body>
-    
+    <header>
+        <h1>titre1</h1>
+    </header>
+    <main>
+        <section>
+            <h2>titre sectioon</h2>
+        </section>
+    </main>
+    <footer>
+        <p>tous droits réservés</p>
+    </footer>
 </body>
 </html>
